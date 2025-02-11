567b93c6fa59519e31480a4cc5b9bc5983b19cd2
Micael <poinenmicael@gmail.com>
2025-02-11 20:16:01 +0100

    ajout d'un code pour MAJ avant d'install le systeme de base

--- install_script.sh
+++ install_script.sh
@@ -66,7 +66,10 @@ mount "/dev/$VG_NAME/$LV_SHARED" "$SHARED_DIR"
 # Activer le swap
 swapon "/dev/$VG_NAME/$LV_SWAP"
 
-# Installation du système de base
+# Installation du système de base avec un verif de maj avant 
+pacman -Sy reflector --noconfirm
+reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+
 pacstrap "$MOUNT_POINT" base base-devel linux linux-firmware lvm2 cryptsetup vim nano sudo networkmanager
 
 # Génération du fstab
